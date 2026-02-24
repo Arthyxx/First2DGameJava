1f75dba12b27a22b7b2d309cdda0d08aa3b89df9
Arthur Gonçalves <arthur.limag.alu.lmb@gmail.com>
2026-02-23 21:48:06 -0300

    chore: initial commit (basic game loop + window)

---
+++ src/main/GamePanel.java
@@ -0,0 +1,52 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while(gameThread != null){
+            // 1 UPDATE: update information such as character position
+            update();
+
+            // 2 DRAW: draw the screen with the updated information
+            repaint();
+        }
+    }
+
+    public void update(){
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(100, 100, this.tileSize, this.tileSize);
+        g2.dispose();
+    }
+}
